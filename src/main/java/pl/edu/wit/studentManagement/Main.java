fc72abf0a61b1f2a33ea6e15d6c17ca7e027b4fc
Michał Zawadzki <51023578+majkelmichel@users.noreply.github.com>
2025-05-23 18:35:13 +0200

    Tyćkę zmian (#11)

--- src/main/java/pl/edu/wit/studentManagement/Main.java
+++ src/main/java/pl/edu/wit/studentManagement/Main.java
@@ -1,10 +1,6 @@
 package pl.edu.wit.studentManagement;
 
-import pl.edu.wit.studentManagement.view.AppWindow;
-
 public class Main {
     public static void main(String[] args) {
-        var mainWindow = new AppWindow();
-        mainWindow.show();
     }
 }
