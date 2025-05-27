3e660148ce1f95823324b03512c9282432c1912d
Michał Pokrzywka <michalpokrzywka0@gmail.com>
2025-05-27 19:29:27 +0200

    Added comments to StudentTranslation and StudentTranslationTest

--- src/main/java/pl/edu/wit/studentManagement/translations/impl/StudentTranslation.java
+++ src/main/java/pl/edu/wit/studentManagement/translations/impl/StudentTranslation.java
@@ -3,15 +3,60 @@ package pl.edu.wit.studentManagement.translations.impl;
 import pl.edu.wit.studentManagement.entities.fields.StudentField;
 import pl.edu.wit.studentManagement.translations.interfaces.Translation;
 
-public class StudentTranslation implements Translation<StudentField> {
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
 
+/**
+ * Translation implementation for {@link StudentField} values.
+ *
+ * <p>This class provides localized (Polish) string representations for
+ * fields related to a student, such as first name, last name, and album number.
+ * It returns a default message for unknown or null fields.</p>
+ *
+ * @author Michał Pokrzywka
+ */
+
+public class StudentTranslation implements Translation<StudentField> {
+    private final ResourceBundle resourceBundle;
+
+    /**
+     * Constructor initializes the ResourceBundle for the specified locale.
+     *
+     * @param locale the locale for which translations should be loaded
+     */
+    public StudentTranslation(Locale locale) {
+        this.resourceBundle = ResourceBundle.getBundle("messages", locale);
+    }
+
+    /**
+     * Returns the localized translation for a given StudentField.
+     *
+     * @param studentField the field to translate
+     * @return the localized string corresponding to the field, or a default value if null
+     */
     @Override
     public String getTranslation(StudentField studentField) {
-        switch (studentField) {
-            // TODO: implement translations
-            default -> {
-                return studentField.getFieldName();
-            }
+        if (studentField == null) {
+            return resourceBundle.getString("student.unknown");
         }
+        String key;
+
+        switch (studentField) {
+            case FIRST_NAME:
+                key = "student.first_name";
+                break;
+            case LAST_NAME:
+                key = "student.last_name";
+                break;
+            case ALBUM:
+                key = "student.album";
+                break;
+            default:
+                key = "student.unknown";
+                break;
+        }
+        return resourceBundle.getString(key);
     }
 }
--- src/test/java/pl/edu/wit/studentManagement/translations/impl/StudentTranslationTest.java
+++ src/test/java/pl/edu/wit/studentManagement/translations/impl/StudentTranslationTest.java
@@ -9,47 +9,71 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit tests for the {@link StudentTranslation} class.
  *
- * <p>These tests verify that the correct Polish translations are returned
+ * <p>These tests verify that the correct Polish and English translations are returned
  * for each {@link StudentField} value, as well as the behavior when a null value is provided.</p>
  *
- * @author Twoje Imię
+ * @author Michał Pokrzywka
  */
 public class StudentTranslationTest {
+    /**
+     * Verifies that the Polish translation for FIRST_NAME is correct.
+     */
     @Test
     void plFirstNameGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("pl"));
         assertEquals("Imię",studentTranslation.getTranslation(StudentField.FIRST_NAME));
     }
+    /**
+     * Verifies that the Polish translation for LAST_NAME is correct.
+     */
     @Test
     void plLastNameGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("pl"));
         assertEquals("Nazwisko",studentTranslation.getTranslation(StudentField.LAST_NAME));
     }
+    /**
+     * Verifies that the Polish translation for ALBUM is correct.
+     */
     @Test
     void plAlbumGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("pl"));
         assertEquals("Numer albumu",studentTranslation.getTranslation(StudentField.ALBUM));
     }
+    /**
+     * Verifies Polish translation for null.
+     */
     @Test
     void plNullGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("pl"));
         assertEquals("Nieznane pole",studentTranslation.getTranslation(null));
     }
+    /**
+     * Verifies that the English translation for FIRST_NAME is correct.
+     */
     @Test
     void enFirstNameGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("en"));
         assertEquals("First name",studentTranslation.getTranslation(StudentField.FIRST_NAME));
     }
+    /**
+     * Verifies that the English translation for LAST_NAME is correct.
+     */
     @Test
     void enLastNameGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("en"));
         assertEquals("Last name",studentTranslation.getTranslation(StudentField.LAST_NAME));
     }
+    /**
+     * Verifies that the English translation for ALBUM is correct.
+     */
     @Test
     void enAlbumGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("en"));
         assertEquals("Student ID",studentTranslation.getTranslation(StudentField.ALBUM));
     }
+    /**
+     * Verifies English translation for null.
+     */
     @Test
     void enNullGetTranslation() {
         StudentTranslation studentTranslation = new StudentTranslation(new Locale("en"));
