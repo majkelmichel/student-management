5846d76c0c1e64285d21bc8ccee692c873e1c377
majkelmichel <mich.zaw2002@gmail.com>
2025-06-20 17:46:02 +0200

    Added package-info.java for packages

---
+++ src/main/java/pl/edu/wit/studentManagement/service/dto/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Contains Data Transfer Object (DTO) classes used to transfer data
+ * between layers of the student management system.
+ * <p>
+ * This package includes DTOs for core entities and is organized into
+ * the following subpackages:
+ * <ul>
+ *   <li>{@code grade} - DTOs related to grades
+ *   <li>{@code gradeCriterion} - DTOs related to grade criteria
+ *   <li>{@code gradeMatrix} - DTOs for grade matrix representations
+ *   <li>{@code student} - DTOs related to students
+ *   <li>{@code studentGroup} - DTOs related to student groups
+ *   <li>{@code subject} - DTOs related to subjects
+ * </ul>
+ * <p>
+ * DTOs in this package are used primarily for data exchange with
+ * external layers such as UI or APIs.
+ *
+ * @author Michał
+ */
+package pl.edu.wit.studentManagement.service.dto;
