4be512c7dc1c84daff77b533db7269f26ab23223
Wojciech Berdowski <wberdowski@outlook.com>
2025-06-23 14:25:52 +0200

    javadoc dla Fragment

--- src/main/java/pl/edu/wit/studentManagement/view/interfaces/Fragment.java
+++ src/main/java/pl/edu/wit/studentManagement/view/interfaces/Fragment.java
@@ -2,6 +2,11 @@ package pl.edu.wit.studentManagement.view.interfaces;
 
 import javax.swing.*;
 
+/**
+ * Interface representing a fragment of the application.
+ *
+ * @author Wojciech Berdowski
+ */
 public interface Fragment {
     JPanel getPanel();
 }
